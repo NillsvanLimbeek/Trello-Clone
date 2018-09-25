9bdf24cef32055ed7e886a831949ddabafac1e92
Nills van Limbeek <nills.van.limbeek@live.nl>
2018-09-25 09:43:37 +0200

    added stuff

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,6 +3,19 @@
         <CalendarHeader />
 
         <div class="calendar__list">
+            <div class="calendar__list-header">
+
+                <div
+                    class="calendar__day-name"
+                    v-for="name in dayNames"
+                    :key="name.id">
+
+                    {{ name }}
+
+                </div>
+
+            </div>
+
             <div
                 class="calendar__week"
                 v-if="calendarView === CalendarView.Week">
@@ -17,12 +30,15 @@
 
                 <div
                     class="calendar__days"
+                    :class="{ inactive: !isSameMonth(day) }"
                     v-for="day in monthDays"
                     :key="day.id">
 
                     <p class="calendar__days-title">
                         {{ day | date('D') }}
                     </p>
+
+                    <svg height="10" width="10" xmlns="http://www.w3.org/2000/svg" version="1.1"> <defs> <pattern id="diagonal-stripe-1" patternUnits="userSpaceOnUse" width="10" height="10"> <image xlink:href="data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScxMCcgaGVpZ2h0PScxMCc+CiAgPHJlY3Qgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJyBmaWxsPSd3aGl0ZScvPgogIDxwYXRoIGQ9J00tMSwxIGwyLC0yCiAgICAgICAgICAgTTAsMTAgbDEwLC0xMAogICAgICAgICAgIE05LDExIGwyLC0yJyBzdHJva2U9J2JsYWNrJyBzdHJva2Utd2lkdGg9JzEnLz4KPC9zdmc+Cg==" x="0" y="0" width="10" height="10"> </image> </pattern> </defs> </svg>
 
                 </div>
 
@@ -56,12 +72,14 @@
         private monthDays: Date[] = [];
         private weekDays: Date[] = [];
 
+        private dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
         private daysInMonth() {
             this.monthDays = [];
             const dayList: Date[] = [];
 
-            const startOfMonth = moment().startOf('month');
-            const endOfMonth = moment().endOf('month');
+            const startOfMonth = moment().startOf('month').startOf('week');
+            const endOfMonth = moment().endOf('month').endOf('week');
 
             while (startOfMonth.diff(endOfMonth) < 0) {
                 dayList.push(startOfMonth.clone().toDate());
@@ -69,6 +87,16 @@
             }
 
             this.monthDays = dayList;
+        }
+
+        private isSameMonth(date: Date) {
+            const today = moment();
+
+            if (moment(date).isSame(today, 'month')) {
+                return true;
+            }
+
+            return false;
         }
 
         private daysInWeek() {
