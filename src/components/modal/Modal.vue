8f39a97b082f6f9704605c098a228d5f3260886e
Nills van Limbeek <nills.van.limbeek@live.nl>
2018-11-09 08:41:57 +0100

    modal grid

--- src/components/modal/Modal.vue
+++ src/components/modal/Modal.vue
@@ -14,14 +14,21 @@
                     <polygon points="31.1 1.4 29.7 0 15.6 14.1 1.4 0 0 1.4 14.1 15.6 0 29.7 1.4 31.1 15.6 17 29.7 31.1 31.1 29.7 17 15.6 "/>
                 </svg>
 
-                <input
+                <div class="modal__title">Title</div>
+                <div class="modal__labels">Labels</div>
+                <div class="modal__description">Description</div>
+                <div class="modal__comments">Comments</div>
+                <div class="modal__activity">Activity</div>
+                <div class="modal__card-actions">Card Actions</div>
+
+                <!-- <input
                     type="text"
                     class="column__input"
                     :placeholder="modal.title">
 
                 <p>
                     In list <span class="modal__list-name">{{ column.title }}</span>
-                </p>
+                </p> -->
             </div>
         </div>
     </transition>
