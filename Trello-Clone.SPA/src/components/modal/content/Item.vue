4559a34cd2a508c65d5745a9c4fc02d9ae4cd2d5
Nills van Limbeek <n.vanlimbeek@timechimp.com>
2018-12-10 20:32:42 +0100

    restructure modal

---
+++ Trello-Clone.SPA/src/components/modal/content/Item.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="modal-item">
+        <div class="modal__title">
+            <div class="modal__icon">
+                <icon-base view-box="0 0 60 60" class="modal-icon">
+                    <icon-folder />
+                </icon-base>
+            </div>
+
+            <div class="modal__content">
+                <input
+                    type="text"
+                    class="modal__input"
+                    :placeholder="modal.title">
+
+                <p class="modal__sub-title">
+                    In list <span class="modal__list-name">{{ column.title }}</span>
+                </p>
+            </div>
+        </div>
+
+        <div class="modal__labels">
+            <div class="modal__icon">
+                <icon-base view-box="0 0 60 60" class="modal-icon">
+                    <icon-label />
+                </icon-base>
+            </div>
+
+            <div class="modal__content">
+                <p class="modal__title">Labels</p>
+                <Labels :labels="modal.colorLabels"/>
+            </div>
+        </div>
+
+        <div class="modal__description">
+            <div class="modal__icon">
+                <icon-base view-box="0 0 22.9 22.9" class="modal-icon">
+                    <icon-description />
+                </icon-base>
+            </div>
+
+            <div class="modal__content">
+                Description
+            </div>
+        </div>
+
+        <div class="modal__comments">
+            <div class="modal__icon">
+                <icon-base view-box="0 0 33 33" class="modal-icon">
+                    <icon-comments />
+                </icon-base>
+            </div>
+
+            <div class="modal__content">
+                Comments
+            </div>
+        </div>
+
+        <div class="modal__activity">
+            <div class="modal__icon">
+                <icon-base view-box="0 0 489.7 489.7" class="modal-icon">
+                    <icon-activity />
+                </icon-base>
+            </div>
+
+            <div class="modal__content">
+                Activity
+            </div>
+        </div>
+
+        <div class="modal__card-actions">Card Actions</div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from '@/vue-script';
+
+    import Labels from './Labels.vue';
+    import {
+        IconActivity,
+        IconBase,
+        IconClose,
+        IconComments,
+        IconDescription,
+        IconFolder,
+        IconLabel,
+    } from '@components/icons';
+
+    import { IItem, IColumn } from '@models/index';
+    import { EventBus } from '@/eventBus';
+
+    @Component({
+        components: {
+            Labels,
+            IconActivity,
+            IconBase,
+            IconClose,
+            IconComments,
+            IconDescription,
+            IconFolder,
+            IconLabel,
+        },
+    })
+    export default class Modal extends Vue {
+        @Prop({ required: true }) private id!: number;
+
+        private closeModal() {
+            EventBus.$emit('close-modal');
+        }
+
+        private get modal() {
+            const item: IItem = this.$store.getters.getItem(this.id);
+            return item;
+        }
+
+        private get column() {
+            const { columnId } = this.modal;
+            const column: IColumn = this.$store.getters.getColumn(columnId);
+            return column;
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Trello-Clone.SPA/src/components/modal/content/NewBoard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="modal-new-board">
+        New Board
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue, Component } from '@/vue-script';
+
+    export default class NewBoard extends Vue {
+
+    }
+</script>
